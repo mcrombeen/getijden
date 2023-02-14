fff0932efd7b9e3406161fb3dffd1564d8c82059
mcr <43091597+mcrombeen@users.noreply.github.com>
2023-02-14 12:15:04 +0100

    Create script.js

---
+++ script.js
@@ -0,0 +1,24 @@
+// Fetch the data from the JSON file
+fetch('data.json')
+  .then(response => response.json())
+  .then(data => {
+    // Find the current date in the data
+    const currentDate = new Date().toLocaleDateString();
+    const currentData = data.find(obj => obj.Date === currentDate);
+
+    // Update the date element
+    const dateElement = document.getElementById('date');
+    dateElement.textContent = currentDate;
+
+    // Update the data elements
+    const dataElement = document.getElementById('data');
+    Object.keys(currentData).forEach(key => {
+      if (key !== 'Date') {
+        const value = currentData[key];
+        const listItem = document.createElement('li');
+        listItem.textContent = `${key}: ${value}`;
+        dataElement.appendChild(listItem);
+      }
+    });
+  })
+  .catch(error => console.error(error));
