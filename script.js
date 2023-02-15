fa2d7750be2e0a15df6a3291ffeea795c3ff2a39
mcr <43091597+mcrombeen@users.noreply.github.com>
2023-02-15 19:41:13 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,68 +1,44 @@
-// Create a new XMLHttpRequest object
-const xhr = new XMLHttpRequest();
-// Set up the request
-xhr.open('GET', 'Antwerpen_Februari.json');
-
-// Handle the response
-xhr.onload = function() {
-  if (xhr.status === 200) {
-    const data = JSON.parse(xhr.responseText);
-    // Find the current date in the data
-    const currentDate = new Date().toLocaleDateString();
-    const currentDay = new Date().toLocaleDateString('nl-BE', {weekday: 'long'});
-
-    const currentData = data.find(obj => obj.Date === currentDate);
-    // Update the date element
-    const dateElement = document.getElementById('date');
-    dateElement.textContent = currentDay + (' ') + currentDate;
-    // Update the data elements
-    const dataElement = document.getElementById('data');
+const showDataBtn = document.getElementById('showDataBtn');
+showDataBtn.addEventListener('click', function() {
+  const selectedDate = document.getElementById('datepicker').value;
+  const currentData = data.find(obj => obj.Date === selectedDate);
+  if (currentData) {
+    // Create a dialog box to display the data
+    const dialog = document.createElement('dialog');
+    const closeBtn = document.createElement('button');
+    closeBtn.textContent = 'Close';
+    closeBtn.addEventListener('click', function() {
+      dialog.close();
+    });
+    dialog.appendChild(closeBtn);
     Object.keys(currentData).forEach(key => {
       if (key !== 'Date') {
         let value = currentData[key];
-    if (key === 'HW1') {
-    key = 'Eerste Hoogwater ';
-    value += ' hr';
-  }
-  else if (key === 'HW2') {
-    key = 'Tweede Hoogwater ';
-    value += ' hr';
-  }
-  else if (key === 'LW1') {
-    key = 'Eerste Laagwater ';
-    value += ' hr';
-  }
-  else if (key === 'LW2') {
-    key = 'Tweede Laagwater ';
-    value += ' hr';
-  }
-  else if (key === 'm TAW') {
-    key = 'm TAW ';
-    value += ' mtr';
-  }
-  else if (key === 'm TAW__1') {
-    key = 'm TAW ';
-    value += ' mtr';
-  }
- else if (key === 'm TAW__2') {
-    key = 'm TAW ';
-    value += ' mtr';
-  }
-  else if (key === 'm TAW__3') {
-    key = 'm TAW ';
-    value += ' mtr';
-  }
-
+        if (key === 'HW1') {
+          key = 'Eerste Hoogwater';
+          value += ' hr';
+        } else if (key === 'HW2') {
+          key = 'Tweede Hoogwater';
+          value += ' hr';
+        } else if (key === 'LW1') {
+          key = 'Eerste Laagwater';
+          value += ' hr';
+        } else if (key === 'LW2') {
+          key = 'Tweede Laagwater';
+          value += ' hr';
+        } else if (key === 'm TAW' || key === 'm TAW__1' || key === 'm TAW__2' || key === 'm TAW__3') {
+          key = 'm TAW';
+          value += ' mtr';
+        }
         const listItem = document.createElement('li');
         listItem.textContent = `${key}: ${value}`;
-        dataElement.appendChild(listItem);
-        dataElement.appendChild(document.createElement('br')); // Insert a line break after each list item
+        dialog.appendChild(listItem);
+        dialog.appendChild(document.createElement('br')); // Insert a line break after each list item
       }
     });
+    document.body.appendChild(dialog);
+    dialog.showModal();
   } else {
-    console.error('Failed to load data from Antwerpen_Februari.json');
+    alert('No data available for the selected date');
   }
-};
-
-// Send the request
-xhr.send();
+});
