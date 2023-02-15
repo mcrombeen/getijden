ca15b8ee9cbf332f45c575635df69c629e69573d
mcr <43091597+mcrombeen@users.noreply.github.com>
2023-02-15 09:25:42 +0100

    Update script.js

--- script.js
+++ script.js
@@ -13,7 +13,7 @@ fetch('Antwerpen_Februari.json')
     Object.keys(currentData).forEach(key => {
       if (key !== 'Date') {
         let value = currentData[key];
-        if (key.match('HW1') {
+        if (key.match("HW1") {
           value += ' hr';
         } else {
           value += ' mtr';
