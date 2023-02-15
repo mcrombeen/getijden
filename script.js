a975b839ba8427ba1bd030c9274ff116571ada31
mcr <43091597+mcrombeen@users.noreply.github.com>
2023-02-15 08:41:16 +0100

    Update script.js

--- script.js
+++ script.js
@@ -5,22 +5,18 @@ fetch('Antwerpen_Februari.json')
     // Find the current date in the data
     const currentDate = new Date().toLocaleDateString();
     const currentData = data.find(obj => obj.Date === currentDate);
-
     // Update the date element
     const dateElement = document.getElementById('date');
     dateElement.textContent = currentDate;
-
     // Update the data elements
     const dataElement = document.getElementById('data');
     Object.keys(currentData).forEach(key => {
       if (key !== 'Date') {
         let value = currentData[key];
-        if (key.match('HW1')) {
-          key == ('Eerste Hoogwater');
+        if (key.match(/^(HW|LW)\d$/)) {
           value += ' hr';
-        }
-         else {
-          value += ' mtr';
+        if (key.match(/^(m TAW__1|m TAW_ 2|m TAW_3))) {
+          value += ' mtr';  
         }
         const listItem = document.createElement('li');
         listItem.textContent = `${key}: ${value}`;
@@ -28,5 +24,5 @@ fetch('Antwerpen_Februari.json')
         dataElement.appendChild(document.createElement('br')); // Insert a line break after each list item
       }
     });
-  
-  
+  })
+  .catch(error => console.error(error));
