7ba628d281f715930b0e5a0086da5fa2de911896
mcr <43091597+mcrombeen@users.noreply.github.com>
2023-02-22 10:29:07 +0100

    Update scriptDatum.js

--- scriptDatum.js
+++ scriptDatum.js
@@ -1,67 +1,55 @@
 document.addEventListener('DOMContentLoaded', function() {
   const xhr = new XMLHttpRequest();
   const datePicker = document.getElementById('datepicker');
-  datePicker.addEventListener('input', function() {
-    xhr.open('GET', `https://raw.githubusercontent.com/mcrombeen/getijden/main/Antwerpen_${datePicker.value}.json`);
-    xhr.onload = function() {
-      if (xhr.status === 200) {
-        const data = JSON.parse(xhr.responseText);
-        const currentDate = new Date(datePicker.value).toLocaleDateString({month: 'long'});
-        const currentDay = new Date(datePicker.value).toLocaleDateString('nl-NL', {weekday: 'long'});
-        const currentData = data.find(obj => obj.Date == currentDate);
-        const dateElement = document.getElementById('date');
-        dateElement.textContent = currentDay + (' ') + currentDate;
-        const dataElement = document.getElementById('data');
-        dataElement.innerHTML = '';
-        Object.keys(currentData).forEach(key => {
-          if (key !== 'Date') {
-            let value = currentData[key];
-            if (key === 'HW1') {
-              key = 'Eerste Hoogwater ';
-              value += ' hr';
+  xhr.open('GET', 'https://raw.githubusercontent.com/mcrombeen/getijden/main/Antwerpen.json');
+  xhr.onload = function() {
+    if (xhr.status === 200) {
+      const data = JSON.parse(xhr.responseText);
+      datePicker.addEventListener('input', function() {
+        const selectedDate = new Date(datePicker.value).toLocaleDateString({month: 'long'});
+        const currentData = data.find(obj => obj.Date === selectedDate);
+        if (currentData) {
+          const currentDay = new Date(datePicker.value).toLocaleDateString('nl-NL', {weekday: 'long'});
+          const dateElement = document.getElementById('date');
+          dateElement.textContent = currentDay + ' ' + selectedDate;
+          const dataElement = document.getElementById('data');
+          dataElement.innerHTML = '';
+          Object.keys(currentData).forEach(key => {
+            if (key !== 'Date') {
+              let value = currentData[key];
+              if (key === 'HW1') {
+                key = 'Eerste Hoogwater ';
+                value += ' hr';
+              } else if (key === 'HW2') {
+                key = 'Tweede Hoogwater ';
+                value += ' hr';
+              } else if (key === 'LW1') {
+                key = 'Eerste Laagwater ';
+                value += ' hr';
+              } else if (key === 'LW2') {
+                key = 'Tweede Laagwater ';
+                value += ' hr';
+              } else if (key === 'm TAW' || key.startsWith('m TAW_')) {
+                key = 'm TAW ';
+                value += ' mtr';
+              }
+              const listItem = document.createElement('li');
+              listItem.textContent = `${key}: ${value}`;
+              dataElement.appendChild(listItem);
+              dataElement.appendChild(document.createElement('br'));
             }
-            else if (key === 'HW2') {
-              key = 'Tweede Hoogwater ';
-              value += ' hr';
-            }
-            else if (key === 'LW1') {
-              key = 'Eerste Laagwater ';
-              value += ' hr';
-            }
-            else if (key === 'LW2') {
-              key = 'Tweede Laagwater ';
-              value += ' hr';
-            }
-            else if (key === 'm TAW') {
-              key = 'm TAW ';
-              value += ' mtr';
-            }
-            else if (key === 'm TAW__1') {
-              key = 'm TAW ';
-              value += ' mtr';
-            }
-            else if (key === 'm TAW__2') {
-              key = 'm TAW ';
-              value += ' mtr';
-            }
-            else if (key === 'm TAW__3') {
-              key = 'm TAW ';
-              value += ' mtr';
-            }
-            const listItem = document.createElement('li');
-            listItem.textContent = `${key}: ${value}`;
-            dataElement.appendChild(listItem);
-            dataElement.appendChild(document.createElement('br'));
-          }
-        });
-      } else {
-        console.error('Failed to load data from Antwerpen_Februari.json');
-      }
-    };
-    xhr.send();
-    console.log(datePicker.value);
-  });
+          });
+        } else {
+          console.error(`No data found for ${selectedDate}`);
+        }
+      });
+    } else {
+      console.error('Failed to load data from Antwerpen.json');
+    }
+  };
+  xhr.send();
 });
+
 
 
 
